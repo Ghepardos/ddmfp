803ea2ef14cc86d069e0b94227d426dc92d37e9b
Ghepardos <dennyronchi93@gmail.com>
2025-05-23 23:48:26 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://ghepardos-skium.hf.space"     # non mettere lo / finale al link
+MFPPSW = "Patatini1!"
 
 
 # Constants
